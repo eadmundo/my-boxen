feeed9b75d0d4c907541d6691fd10a4ae54fd1de
Edward Stone <edwardjstone@yahoo.com>
2013-03-07 22:25:20 +0000

    sublime text preferences

--- modules/people/manifests/eadmundo.pp
+++ modules/people/manifests/eadmundo.pp
@@ -25,4 +25,23 @@ class people::eadmundo {
     mode => '0644',
   }
 
+  $application_support_dir = "${homedir}/Library/Application Support"
+
+  $sublime_application_support_dir = "${application_support_dir}/Sublime Text 2"
+
+  $sublime_packages_dir = "${sublime_application_support_dir}/Packages"
+
+  $sublime_user_prefs_dir = "${sublime_packages_dir}/User"
+
+  $sublime_dirs = [$sublime_application_support_dir, $sublime_packages_dir, $sublime_user_prefs_dir]
+
+  file { $sublime_dirs :
+    ensure => directory,
+  }
+
+  repository { $sublime_user_prefs_dir:
+    source  => "${::github_login}/sublime.d",
+    require => File[$sublime_dirs],
+  }
+
 }
